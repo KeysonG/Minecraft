aaab6e4114ff838d317766f7b5c617a58f38a353
KeysonG <gideon.keyson@gmail.com>
2017-01-03 17:32:35 +0200

    tree placement added

--- js/script.js
+++ js/script.js
@@ -24,13 +24,14 @@ function createDivMatrix(){
 
 
 function buildGroundArray(){
-    var maxHeight = 4;
+    var maxHeight = 3;
       for(var i =0;i<containerW/cellWH;i++){
     	var height = Math.floor(Math.random() * maxHeight)+1;
     		groundArray.push(height);
         for(var j= ((containerH/cellWH)-1); j>(((containerH/cellWH)-1) -groundArray[i]); j--){
             divArray[j][i].css("background-image","url('assets/blocks/dirt.png')").attr("colored",true);
-            if(j===(12-groundArray[i])){
+            
+            if(j===(containerH/cellWH-groundArray[i])){
             	divArray[j][i].css("background-image","url('assets/blocks/grass.png')").attr("colored",true);
             }
         }
@@ -39,18 +40,39 @@ function buildGroundArray(){
 }
 
 function placeTree(){
-    var treePosition = Math.floor(Math.random()*containerW);
-    return groundArray[treePosition]+1;
+	var goodTree = false;
+    while(!goodTree){
+    	x = Math.floor(Math.random()*containerW/cellWH);
+    	y = 10-groundArray[x]+1;
+    	goodTree = checkTree(x,y);
+    }
+    divArray[y][x].css("background-image","url('assets/blocks/tree.png')").attr("colored",true);
+
+    // divArray[j][i].css("background-image","url('assets/blocks/tree.png')").attr("colored",true);
+    // return  divArray[j][i];
+
+
+
+    // var treePosition = Math.floor(Math.random()*containerW);
+    // return groundArray[treePosition]+1;
 }
 
-function buildTree(){
-    var treeStartX = placeTree();
-    var treeStartY = placeTree
+function checkTree(x,y){
+	if(y<=(11-groundArray[x-1]) && y<=(11-groundArray[x+1]) ){
+		console.log("true");
+		return true;
+	}
+	else{
+		console.log("false");
+		return false;
+	}
+
 }
 
 function init(){
 	createDivMatrix();
 	buildGroundArray();
+	placeTree();
 }
 
 init();
